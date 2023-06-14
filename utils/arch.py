f98253c1aba4ef3d205ad88e152ee3c19d660f46
Aditya Kumar <adlokib@gmail.com>
2023-06-14 09:03:30 +0530

    Added comments for various files

--- utils/arch.py
+++ utils/arch.py
@@ -5,6 +5,7 @@ from torch import Tensor
 import math
 import numpy as np
 
+# 1D positional encoding
 class PositionalEncoding(nn.Module):
 
     def __init__(self, emb_size: int, maxlen: int = 5000):
@@ -19,7 +20,8 @@ class PositionalEncoding(nn.Module):
 
     def forward(self, token_embedding: Tensor):
         return (token_embedding + self.pos_embedding[:token_embedding.size(0),:])
-    
+
+# Generates n dimentional embedding for a token
 class TokenEmbedding(nn.Module):
     
     def __init__(self, vocab_size: int, emb_size):
@@ -30,8 +32,10 @@ class TokenEmbedding(nn.Module):
     def forward(self, tokens: Tensor):
         return self.embedding(tokens.long()) * math.sqrt(self.emb_size)
 
+# Main model class
 class Network(nn.Module):
 
+    # Generates 2D positional embedding
     def pos_embed_2d(self,shape_2d, dims):
     
         y, x = shape_2d
@@ -55,6 +59,7 @@ class Network(nn.Module):
 
         return res
 
+    # Generates mask for decoder to use
     def generate_square_subsequent_mask(self,sz):
         mask = (torch.triu(torch.ones((sz, sz), device=self.device)) == 1).transpose(0, 1)
         mask = mask.float().masked_fill(mask == 0, float('-inf')).masked_fill(mask == 1, float(0.0))
@@ -65,55 +70,84 @@ class Network(nn.Module):
         
         super().__init__()
 
+        # Dimension of one embedding
         self.dims = 128
+
+        # Vocabulary size
+        self.vocab_len = 99
+
+        # Device o be used
         self.device = device
 
+        # height and width of input after passing through CNN backbone, will be updated at forward pass
         self.h = 0
         self.w = 0
         
+        # CNN backbone to generate patch embeddings from image. Excluding last downsample to get patch embeddings for 16x16 patches
         self.backbone = nn.Sequential(*(list(torchvision.models.mobilenet_v3_small(weights='DEFAULT').children())[:-2][0][:-4]))
+        
+        # 1x1 conv layer to alter channels to required dimensions
         self.conv = nn.Conv2d(in_channels=48, out_channels=self.dims, kernel_size=1)
         self.bn = nn.BatchNorm2d(self.dims)
 
+        # transformer encoders
         encoder_layer = nn.TransformerEncoderLayer(d_model=self.dims, nhead=4,dim_feedforward=512)
         self.transformer_encoder = nn.TransformerEncoder(encoder_layer, num_layers=3).to(device)
         
+        # transformer decoders
         decoder_layer = nn.TransformerDecoderLayer(d_model=self.dims, nhead=4,dim_feedforward=512)
         self.transformer_decoder = nn.TransformerDecoder(decoder_layer, num_layers=3).to(device)
         
+        # 1D positional encoding for character sequence
         self.positional_encoding = PositionalEncoding(self.dims).to(device)
-        self.tok_emb = TokenEmbedding(99, self.dims)
+        
+        # Convertes token to embedding. 4 special tokens + 95 ASCII characters
+        self.tok_emb = TokenEmbedding(self.vocab_len, self.dims)
 
+        # Src mask and pad maask, will be updated at forward pass
         self.src_mask = None
         self.src_pad_mask = None
 
-        self.fc = nn.Linear(in_features=self.dims, out_features=99)
+        # Final linear layer to convert to vocab dimensional output
+        self.fc = nn.Linear(in_features=self.dims, out_features=self.vocab_len)
     
     def forward(self,x,y,trg_pad_mask):
 
+        # Height and width calculated
         self.h = int(np.ceil(x.shape[2]/16))
         self.w = int(np.ceil(x.shape[3]/16))
         
+        # input passed through backbone and 1x1 conv
         t1 = self.backbone(x)
         t1 = self.conv(t1)
         t1 = self.bn(t1)
        
+        # Dimensions permuted to have embeddings at the end
         t1 = t1.permute(0,2,3,1)
 
+        # 2D positional encoding added to CNN output
         inp = t1 + self.pos_embed_2d((self.h,self.w),self.dims).to(self.device)
+        
+        # Unrolled to remove width height info, as 2D positional info is now present
         inp = inp.reshape(-1,self.h*self.w,self.dims) 
+
+        # Dimensions permuted to have number of embeddings first, and batch size second. (Normal transformer format)
         inp = inp.permute((1,0,2))
         
+        # Source mask and pad mask generated
+        self.src_mask = torch.zeros((self.h*self.w,self.h*self.w), dtype=torch.bool).to(self.device)
+        self.src_pad_mask = torch.zeros((x.shape[0],self.h*self.w), dtype=torch.bool).to(self.device)
+
+        # Output tokenized and 1D positional embedding added
         y = self.positional_encoding(self.tok_emb(y))
         len_y = y.shape[0]
         y_mask = self.generate_square_subsequent_mask(len_y)
         
-        self.src_mask = torch.zeros((self.h*self.w,self.h*self.w), dtype=torch.bool).to(self.device)
-        self.src_pad_mask = torch.zeros((x.shape[0],self.h*self.w), dtype=torch.bool).to(self.device)
-
+        # Activations passed through encoder and decoder with appropriate masks and pad masks
         memory = self.transformer_encoder(inp, self.src_mask, self.src_pad_mask[:trg_pad_mask.shape[0],:])
         outs = self.transformer_decoder(y, memory, y_mask, None, trg_pad_mask, self.src_pad_mask[:trg_pad_mask.shape[0],:])
 
+        # Output turned into vocab length dimensional
         return self.fc(outs)
     
     def encode(self, src: Tensor):
--- utils/helper.py
+++ utils/helper.py
@@ -2,23 +2,25 @@ import cv2
 import torch
 import numpy as np
 
-chars = [chr(x) for x in range(32,127)]
-char_dict = {i+4:x for i,x in enumerate(chars)}
+# Character dictionary and reverse character dictionary created
+all_chars = [chr(x) for x in range(32,127)]
+char_dict = {i+4:x for i,x in enumerate(all_chars)}
 char_dict[0] = 'unk'
 char_dict[1] = 'pad'
 char_dict[2] = 'bos'
 char_dict[3] = 'eos'
 rev_char = {y:x for x,y in char_dict.items()}
 
+# Collate function for Dataloader to utilize
 def generate_batch(data_batch,w,h):
     
+    # initializations
     im_list, txt_tokens = [], []
-    
     trg_pad_mask = []
-    
     max_idx = -1
-    
     max_len = 0
+
+    # finding out max length of sequence in batch
     for _,txt_path in data_batch:
         with open(txt_path)as f:
             ann = f.readline()
@@ -28,14 +30,19 @@ def generate_batch(data_batch,w,h):
             
     max_len+=3
     
+    # Iterating over entries in batch
     for im_path,txt_path in data_batch:
         
         entry_txt_tokens = []
         
+        # Reading image and resizing, also BGR to RGB
         im = cv2.cvtColor(cv2.resize(cv2.imread(im_path),(w,h)),cv2.COLOR_BGR2RGB)
 
+        # Reading transcription for corresponding image
         with open(txt_path)as f:
             ann = f.readline()
+
+            # converting transcription from ascii text to numerical representation
             chars = [rev_char[x] for x in list(ann)]
             
         num_chars = len(chars)
@@ -44,17 +51,20 @@ def generate_batch(data_batch,w,h):
         if num_chars+1 > max_idx:
             max_idx = num_chars+1
 
+        # Mask for entry. With padding if not longest entry in batch
         temp_mask = []
         temp_mask.extend([False for _ in range(num_chars+2)])
         temp_mask.extend([True for _ in range(num_pad)])
-        
         trg_pad_mask.append(temp_mask)
+
+        # Token sequence for transcription. 2 as bos, 3 as eos and 1 as pad
         entry_txt_tokens = [2]+chars+[3]+[1 for _ in range(num_pad)]
         
         txt_tokens.append(entry_txt_tokens)
         
         im_list.append(im)
         
+    # Torch tensor created and perrmuted to B,C,H,W format
     im_list = torch.tensor(np.array(im_list)).permute((0,3,1,2))
     
     txt_tokens = torch.tensor(txt_tokens)[:,:max_idx+1].permute((1,0))
